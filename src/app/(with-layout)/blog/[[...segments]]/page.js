7283c627fd567c3895c5b63431e92986fc4ce249
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-09-19 22:04:20 +0600

    catch all route with root folder

--- src/app/(with-layout)/blog/[[...segments]]/page.js
+++ src/app/(with-layout)/blog/[[...segments]]/page.js
@@ -1,9 +1,10 @@
 
 const SingleBlog = ({params}) => {
+    const [year, id] = params.segments || [];
     return (
         <div>
             <h2 className='text-2xl font-bold text-yellow-400 text-center'> Single Blog </h2>
-            <p>{params.id}</p>
+            <p>{year || new Date().getFullYear()} For id {id}</p>
         </div>
     );
 };

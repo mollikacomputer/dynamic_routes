5bc71b197ced8f87971aaf6326af497743a4f6da
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-09-22 20:24:52 +0600

    some change

--- src/app/(with-layout)/blog/[id]/page.js
+++ src/app/(with-layout)/blog/[id]/page.js
@@ -1,12 +1,19 @@
+import loadBlogsData from "@/utils/loadBlogsData";
 import loadSingleBlogData from "@/utils/loadSingleBlogData";
 import Link from "next/link";
+
 export const generateMetadata = async ({params}) =>{
     const {title, body} = await loadSingleBlogData(params.id);
     return{
         title:title,
         description:body,
     }
-};
+}
+
+export const generateStaticParams = async () =>{
+    const blogs = await loadBlogsData();
+    return blogs.map(({id})=> ({id:id.toString()}))
+}
 const SingleBlog = async ({params}) => {
 
     const {id, title, body} = await loadSingleBlogData(params.id);
